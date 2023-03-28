2b090321f5f699619194805eec70dcee0f0386a7
brulogatti <bru.logatti@gmail.com>
2023-03-28 17:07:21 -0300

    Exemplo - Estruturas de Controle JS

---
+++ exemplos.js
@@ -0,0 +1,54 @@
+/*
+Estruturas de Controle do JavaScript
+*/
+
+//Estrutura For
+//Atalho: for + tab
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+//Estrutura While
+//Atalho: while + tab
+while (condition) {
+    
+}
+
+//Estrutura Do...While
+//Atalho: dowhile + tab
+do {
+    
+} while (condition);
+
+//Estrutura Switch Case
+//Atalho: switch + tab
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+//Estrutura For In
+//Atalho: forin + tab
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+//Estrutura For Of
+//Atalho: forof + tab
+for (const iterator of object) {
+    
+}
+
+//Estrutura Foreach
+//Atalho: foreach + tab
+array.forEach(element => {
+    
+});
